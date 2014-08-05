91c708f63e95e235a005a288445c1855b5a3b9d9
Lukasz Roth <lukasz.roth@gmail.com>
2014-08-05 15:33:35 +0200

    bootstrap and fos user bundle

--- BtnAdminBundle.php
+++ BtnAdminBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BtnAdminBundle extends Bundle
 {
+    /**
+     * @return string The Bundle parent name it overrides or null if no parent
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
