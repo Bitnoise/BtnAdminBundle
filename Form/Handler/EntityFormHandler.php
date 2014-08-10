837bacd91bf1b87bb0854e7a4ccd63d43d963cc4
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-10 09:22:46 +0200

    add simple entity form handler

---
+++ Form/Handler/EntityFormHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Btn\AdminBundle\Form\Handler;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManager;
+
+/**
+ *
+ */
+class DefaultFormHandler
+{
+    /** \Doctrine\ORM\EntityManager $em */
+    private $em;
+    /** \Symfony\Component\HttpFoundation\Request $requset */
+    protected $request;
+
+    /**
+     *
+     */
+    public function __construct(EntityManager $em, Request $request = null)
+    {
+        $this->em      = $em;
+        $this->request = $request;
+    }
+
+    /**
+     *
+     */
+    public function handle(FormInterface $form, Request $request = null)
+    {
+        if (!$this->request || !$request) {
+            throw new \Exception('No request pased to handler');
+        }
+
+        $form->handleRequest($request ? $request : $this->request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            if (!$data->getId()) {
+                $this->em->persist($data);
+            }
+
+            $this->em->flush($data);
+
+            return true;
+        }
+    }
+}
