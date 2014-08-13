6347f0eccdf3c9964c49d72e33e91b4c6b71ba78
proszek <proszek@iMac-proszek.local>
2014-08-13 17:03:13 +0200

    JS: Bring back change jstree event behavior.

--- Resources/public/js/nodes.js
+++ Resources/public/js/nodes.js
@@ -1,4 +1,9 @@
 $('#tree')
+    .on('changed.jstree', function (e, data) {
+        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#') {
+            window.location = data.node.a_attr.href;
+        };
+    })
     .jstree({
     "core" : {
         "animation" : 0
