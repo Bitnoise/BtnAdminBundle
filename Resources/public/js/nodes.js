e524419e66a309fb95b061e0f9014453fb8c6fe2
proszek <proszek@iMac-proszek.local>
2014-08-13 10:11:45 +0200

    Added public resources from nodes bundle - js tree.

---
+++ Resources/public/js/nodes.js
@@ -0,0 +1,10 @@
+$('#tree')// listen for event
+    .on('changed.jstree', function (e, data) {
+        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#') {
+            window.location = data.node.a_attr.href;
+        };
+    }).jstree({
+    "core" : {
+        "animation" : 0
+    }
+});
