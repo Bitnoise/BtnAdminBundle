6761eb2839ace55e0ebfa0d6095270f5da5d96fc
proszek <proszek@iMac-proszek.local>
2014-08-14 10:46:28 +0200

    Improved selector for binding jstree.

--- Resources/public/js/nodes.js
+++ Resources/public/js/nodes.js
@@ -1,11 +1,14 @@
-$('#tree')
-    .on('changed.jstree', function (e, data) {
-        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#') {
-            window.location = data.node.a_attr.href;
-        };
-    })
+$('[bind-jstree]')
     .jstree({
     "core" : {
         "animation" : 0
     }
 });
+
+$('[data-change-jstree]')// listen for event
+    .on('changed.jstree', function (e, data) {
+        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#' && e.target.getAttribute('data-change-jstree') === 'reload') {
+            window.location = data.node.a_attr.href;
+        };
+    })
+;
