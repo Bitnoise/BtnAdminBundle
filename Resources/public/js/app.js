067cc752570612ad4a0424b149b24eb6175e7f2b
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-16 21:38:10 +0200

    add app js with pubsub to javascript_head

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -1,8 +1,3 @@
-// subscribe event callback test
-PubSub.subscribe('btn_admin.is_ready', function(e, data) {
-    BtnAdminApp.tools.log(data.message);
-})
-
 // main app object
 var BtnAdminApp = {
     data: {
@@ -10,7 +5,7 @@ var BtnAdminApp = {
     },
     init: function() {
         BtnAdminApp.tools.log('init main admin app');
-        PubSub.publish('btn_admin.is_ready', {message: 'hello world'})
+        PubSub.publish('btn_admin.is_ready');
     },
 };
 
@@ -20,6 +15,3 @@ BtnAdminApp.tools = {
         BtnAdminApp.data.debug ? console.log(msg) : null;
     }
 }
-
-// launch app
-BtnAdminApp.init();
