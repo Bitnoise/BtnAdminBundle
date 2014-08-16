57bf19c5e93543c69199aadb0aee13ed9c1e4f46
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-17 01:38:27 +0200

    add methods to index action

--- Controller/BaseCrudController.php
+++ Controller/BaseCrudController.php
@@ -11,7 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class BaseCrudController extends AbstractCrudController
 {
     /**
-     * @Route("/")
+     * @Route("/", methods={"GET", "POST"})
      */
     public function indexAction(Request $request)
     {
