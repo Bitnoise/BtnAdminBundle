af84f4e357badf08cd844c5c34ca9b93f95ba64c
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-21 16:15:12 +0200

    add parent clall in text type (just in case)

--- Form/Extension/TextTypeExtension.php
+++ Form/Extension/TextTypeExtension.php
@@ -14,6 +14,8 @@ class TextTypeExtension extends AbstractTypeExtension
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
+        parent::buildView($view, $form, $options);
+
         $view->vars['addon_prepend'] = isset($options['addon_prepend']) ? $options['addon_prepend'] : null;
         $view->vars['addon_append'] = isset($options['addon_append']) ? $options['addon_append'] : null;
         $view->vars['addon'] = (!empty($options['addon_prepend']) || !empty($options['addon_append'])) ? true : false;
@@ -24,6 +26,8 @@ class TextTypeExtension extends AbstractTypeExtension
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
+        parent::setDefaultOptions($resolver);
+
         $resolver->setDefaults(array(
             'addon_prepend'  => null,
             'addon_append' => null,
