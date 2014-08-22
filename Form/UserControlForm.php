1f3b8fb555fdf2567db8d9b11c50dc4a31c78abe
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-22 21:58:21 +0200

    clean up userControl form

--- Form/UserControlForm.php
+++ Form/UserControlForm.php
@@ -17,20 +17,19 @@ class UserControlForm extends AbstractForm
             ->add('email', null, array(
                 'label' => 'btn_admin.user.email',
             ))
-            // todo: fix crud form theme (BtnAdminBundle:Form:fields.html.twig) to handle the 'repeated' label block
             ->add('plainPassword', 'repeated', array(
                 'type'            => 'password',
-                // 'options'         => array('translation_domain' => 'FOSUserBundle'),
-                'first_options'   => array('label' => 'btn_admin.user.password',
-                                           'label_attr' => array('class' => 'col-sm-2 control-label')),
-                'second_options'  => array('label' => 'btn_admin.user.password_confirm',
-                                           'label_attr' => array('class' => 'col-sm-2 control-label')),
+                'first_options'   => array(
+                    'label' => 'btn_admin.user.password'
+                ),
+                'second_options'  => array(
+                    'label' => 'btn_admin.user.password_confirm'
+                ),
                 'invalid_message' => 'fos_user.password.mismatch',
             ))
             ->add('enabled', null, array(
                 'label' => 'btn_admin.user.enabled',
             ))
-            ->add('save', $options['data']->getId() ? 'btn_update' : 'btn_create')
         ;
     }
 
