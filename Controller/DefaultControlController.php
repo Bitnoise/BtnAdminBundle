5c184e39258e9ad9548974d9c2cee7f30d120ffa
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-23 09:12:08 +0200

    clean up default control conttoller

--- Controller/DefaultControlController.php
+++ Controller/DefaultControlController.php
@@ -6,16 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-/**
- * @Route()
- */
 class DefaultControlController extends Controller
 {
     /**
-     * The default admin panel view
-     *
-     * @Route("/", name="btn_admin_homepage")
-     * @Route("/", name="btn_admin_index")
+     * @Route("/", name="btn_admin_defaultcontrol_index")
      * @Template()
      */
     public function indexAction()
