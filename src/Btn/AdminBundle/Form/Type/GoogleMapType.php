9df7bc9ea09c0a449a160c9594b6a66758182e87
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-24 17:19:43 +0200

    add google map type

---
+++ src/Btn/AdminBundle/Form/Type/GoogleMapType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Btn\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GoogleMapType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $this->assetLoader->load(array('btn_admin_jquery_ui', 'btn_admin_addresspicker_js'));
+
+        $builder
+            ->add('address', $options['adress_field'] ? 'text' : 'hidden', array(
+                'required' => true,
+                'label'    => false,
+            ))
+            ->add('route', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('streetNumber', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('country', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('locality', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('sublocality', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('postalCode', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('lat', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('lng', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('zoom', 'hidden', array(
+                'required' => false,
+            ))
+            ->add('maptypeid', 'hidden', array(
+                'required' => false,
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setOptional(array(
+            'adress_field',
+        ));
+
+        $resolver->setDefaults(array(
+            'adress_field' => true,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'btn_google_map';
+    }
+}
