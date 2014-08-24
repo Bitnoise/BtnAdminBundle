e1377e69a5ec92986f6263414515287f6a81c6d0
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-24 09:58:26 +0200

    add mapping to User entity

--- Entity/User.php
+++ Entity/User.php
@@ -2,10 +2,15 @@
 
 namespace Btn\AdminBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Btn\AdminBundle\Model\AbstractUser;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- *
+ * @ORM\Entity()
+ * @ORM\Table(name="btn_user")
+ * @UniqueEntity(fields="email")
+ * @UniqueEntity(fields="username")
  */
 class User extends AbstractUser
 {
