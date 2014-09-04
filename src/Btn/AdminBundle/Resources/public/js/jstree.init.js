cbbcba7b9b2427fc14e462449679054e7bae8f5e
proszek <proszek@iMac-proszek.local>
2014-09-04 16:51:09 +0200

    Created js logic to handle Drag and Drop ( jstree plugin ): - Disable dnd at start - Trigger disabel on button click. Added simple class toggle ( color ). - Allow dnd only if opertion is move and if node has draggable attr set to true - avoid to sort root nodes.

--- src/Btn/AdminBundle/Resources/public/js/jstree.init.js
+++ src/Btn/AdminBundle/Resources/public/js/jstree.init.js
@@ -1,13 +1,30 @@
 /* global BtnApp, jQuery */
 (function(app, $, undefined){
     'use strict';
+    //diable drag and drop at start
+    var allowDnD = false;
     var addEvents = function(context) {
         app.tools.findOnce('btn-jstree', context).each(function() {
             var element = $(this);
             element.jstree({
-                'core' : {
-                    'animation' : 0
-                }
+                'core': {
+                    'animation' : 0,
+                    'check_callback' : function (operation, node, node_parent, node_position, more) {
+                        // operation can be 'create_node', 'rename_node', 'delete_node', 'move_node' or 'copy_node'
+                        //if operation is move_node and node is draggable
+                        if (operation === 'move_node' && node.data.jstree.draggable && allowDnD) {
+
+                            return true;
+                        };
+
+                        return false;
+                    }
+                },
+                // 'dnd': {
+                //     'is_draggable': true
+                // },
+                'plugins' : ['dnd'],
+
             });
             if (element.attr('btn-jstree-change')) {
                 element.on('changed.jstree', function (e, data) {
@@ -16,11 +33,34 @@
                     }
                 });
             }
+            element.on('move_node.jstree', function (e, data) {
+                //ping back-end
+                // console.log(data.parent);
+            });
+        });
+    };
+
+    var startDnD = function(context) {
+        var trigger = $('[btn-jstree-startdnd]');
+        $(context).on('click', trigger.selector, function() {
+            allowDnD = !allowDnD;
+            trigger.toggleClass('btn-success').toggleClass('btn-danger');
+            // .toggle(
+            //     function(){
+            //       $(this).text('start');
+            //     },
+            //     function(){
+            //       $(this).text('stop');
+            //     }
+            // );
+
+            return false;
         });
     };
 
     app.init(function(msg, data) {
         addEvents(data.context);
+        startDnD(data.context);
     });
 
     app.refresh(function(msg, data) {
@@ -28,3 +68,5 @@
     });
 
 })(BtnApp, jQuery);
+
+
