9c3f32a2e412db87b4e9d821b7e93c114aced6b4
Adam Misiorny <adam.misiorny@gmail.com>
2015-02-26 16:06:33 +0100

    add btn_hr form type for separator

---
+++ src/Btn/AdminBundle/Form/Type/HrType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Btn\AdminBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class HrType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'required' => false,
+            'label'    => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'btn_hr';
+    }
+}
