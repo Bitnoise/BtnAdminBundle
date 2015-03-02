120ba1d008337a5b7a725eb6038241fd8e5870f0
Adam Misiorny <adam.misiorny@gmail.com>
2015-03-02 17:19:22 +0100

    add btn_youtube form type

---
+++ src/Btn/AdminBundle/Form/DataTransformer/YoutubeTransformer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Btn\AdminBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Btn\AdminBundle\ValueObject\Youtube;
+
+class YoutubeTransformer implements DataTransformerInterface
+{
+    /**
+     *
+     */
+    public function transform($video)
+    {
+        if (null === $video) {
+            return "";
+        }
+
+        return Youtube::create($video);
+    }
+
+    /**
+     *
+     */
+    public function reverseTransform($video)
+    {
+        if (!$video) {
+            return;
+        }
+
+        return (string) $video;
+    }
+}
---
+++ src/Btn/AdminBundle/Form/Type/YoutubeType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Btn\AdminBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Btn\BaseBundle\Util\Youtube;
+use Btn\AdminBundle\Form\DataTransformer\YoutubeTransformer;
+
+class YoutubeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        if ('Btn\AdminBundle\ValueObject\Youtube' === $options['data_class']) {
+            $builder->addModelTransformer(new YoutubeTransformer());
+        }
+
+        if ($options['convert_to_id']) {
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+                $data = $event->getData();
+
+                $id = Youtube::getVideoId($data);
+                if (null !== $id) {
+                    $event->setData($id);
+                }
+            });
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['preview'] = $options['show_thumbnail'] ? true : false;
+        $view->vars['preview_url'] = 'https://www.youtube.com/embed/';
+        $view->vars['preview_width'] = 320;
+        $view->vars['preview_height'] = 180;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setOptional(array(
+            'convert_to_id',
+            'show_thumbnail',
+        ));
+
+        $resolver->setDefaults(array(
+            'data_class'     => 'Btn\AdminBundle\ValueObject\Youtube',
+            'convert_to_id'  => true,
+            'show_thumbnail' => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'btn_youtube';
+    }
+}
