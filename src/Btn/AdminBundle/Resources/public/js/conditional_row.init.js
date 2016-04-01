e93de1cf18e3e49d48e6dfb307c7a452f9632a8a
Adam Misiorny <adam.misiorny@gmail.com>
2016-04-01 11:37:55 +0200

    remix for empty reference value

--- src/Btn/AdminBundle/Resources/public/js/conditional_row.init.js
+++ src/Btn/AdminBundle/Resources/public/js/conditional_row.init.js
@@ -9,21 +9,19 @@
                 var condGroup = element.attr('btn-has-conditional-rows');
                 // hide all from group
                 var condRows = $('[btn-conditional-row-name='+condGroup+']');
-                var referenceValue = element.val()
-                var condRowsFiltered = referenceValue ? condRows.filter('[btn-conditional-row-value='+referenceValue+']') : null
+                var referenceValue = element.val() || '';
+                var condRowsFiltered = condRows.filter('[btn-conditional-row-value="'+referenceValue+'"]');
                 if (element.attr('type') === 'checkbox') {
                     if (element.attr('readonly') && !element.is(':checked')) {
                         // don't react on disabling readonly field
-                    } else if (condRowsFiltered) {
+                    } else if (condRowsFiltered.length) {
                         condRowsFiltered.toggleClass('hidden', !element.is(':checked'));
                     } else {
                         condRows.addClass('hidden');
                     }
                 } else {
                     condRows.addClass('hidden');
-                    if (condRowsFiltered) {
-                        condRowsFiltered.removeClass('hidden');
-                    }
+                    condRowsFiltered.removeClass('hidden');
                 }
             }).trigger('change');
         });
