2575b484d19b449adf14e76584f085303dc23a51
lduraj90 <lukasz.d@bitnoi.se>
2017-03-02 13:15:40 +0100

    add select2 ajax type (#3)

---
+++ src/Btn/AdminBundle/Form/Type/Select2AjaxType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Btn\AdminBundle\Form\Type;
+
+use Btn\BaseBundle\Util\Form;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * {@inheritdoc}
+ */
+class Select2AjaxType extends Select2Type
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefined(array(
+            'data_route_path',
+            'cache',
+            'min_input_length'
+        ));
+
+        /**
+         * Route path should return ajax response:
+         * [
+         *    {
+         *      id: 'id',
+         *      text: 'text'
+         *    }
+         * ]
+         */
+        $resolver->setAllowedTypes('data_route_path', array('string', 'null'));
+        $resolver->setAllowedTypes('cache', array('bool'));
+        $resolver->setAllowedTypes('min_input_length', array('integer'));
+
+        $resolver->setDefaults([
+            'data_route_path' => null,
+            'cache' => true,
+            'min_input_length' => 3
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $viewData = $form->getViewData();
+        if(isset($viewData['id']) && isset($viewData['text'])){
+            $view->vars['attr']['btn-select2-ajax-id'] = $viewData['id'];
+            $view->vars['attr']['btn-select2-ajax-text'] = $viewData['text'];
+            $view->vars['value'] = null;
+        }
+        $view->vars['attr']['btn-select2-is-ajax'] = 1;
+        $view->vars['attr']['btn-select2-route-path'] = $this->router->getRouteCollection()->get($options['data_route_path'])->getPath();
+        $view->vars['attr']['btn-select2-ajax-cache'] = $options['cache'];
+        $view->vars['attr']['btn-select2-min-length'] = $options['min_input_length'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAlias()
+    {
+        return 'ajax';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return Form::getFormName(array(
+            'alias' => $this->getAlias(),
+            'class' => TextType::class,
+        ));
+    }
+}
--- src/Btn/AdminBundle/Resources/public/js/select2.init.js
+++ src/Btn/AdminBundle/Resources/public/js/select2.init.js
@@ -5,45 +5,112 @@
         app.tools.findOnce('btn-select2', context).each(function() {
             var element = $(this);
             var options = element.attr('btn-select2-options');
+            var isAjax = element.attr('btn-select2-is-ajax');
             if ('string' === typeof options) {
                 options = $.parseJSON(options);
             }
             options = options || {};
 
-            if (element.is('[btn-select2-minimum-results-for-search]') && !('minimumResultsForSearch' in options)) {
-                options.minimumResultsForSearch = parseInt(element.attr('btn-select2-minimum-results-for-search'), 10);
-            }
+            if (!isAjax) {
+                if (element.is('[btn-select2-minimum-results-for-search]') && !('minimumResultsForSearch' in options)) {
+                    options.minimumResultsForSearch = parseInt(element.attr('btn-select2-minimum-results-for-search'), 10);
+                }
 
-            if (element.is('[btn-select2-tree]') && !('formatResult' in options)) {
-                options.formatResult = function(state) {
-                    if (!state.id) {
-                        return state.text; // optgroup
+                if (element.is('[btn-select2-tree]') && !('formatResult' in options)) {
+                    options.formatResult = function(state) {
+                        if (!state.id) {
+                            return state.text; // optgroup
+                        }
+                        var text = state.text;
+
+                        text = text.replace('|_', '&nbsp;');
+                        text = text.replace(/^[\_]+/, function(found) {
+                            return new Array(found.length * 4).join('&nbsp;');
+                        });
+
+                        return text;
+                    };
+                } else if (!('formatResult' in options)) {
+                    options.formatResult = function(state) {
+                        return '<span btn-select2-value="'+state.id+'">'+state.text+'</span>';
+                    };
+                }
+
+                if (!('formatSelection' in options)) {
+                    options.formatSelection = function(state) {
+                        var parent = $(state.element).parent();
+                        var text = state.text.replace(/^[\_|]+/, '');
+                        if ('OPTGROUP' === parent.prop('tagName') && parent.attr('label')) {
+                            return  text + ' <small class="optgroup">('+parent.attr('label')+')</small>';
+                        }
+
+                        return text;
+                    };
+                }
+            } else {
+                var routePath = element.attr('btn-select2-route-path');
+                options. minimumInputLength = 3;
+
+                var prefix = Date.now(),
+                    cache = {};
+                options.ajax = {
+                    url: routePath,
+                    dataType: 'json',
+                    delay: 750,
+                    transport: function(params) {
+                        var success = params.success,
+                            failure = params.error;
+                        if (element.attr('btn-select2-ajax-cache')) {
+                            var key = prefix + ' page:' + 1 + ' ' + params.data.q,
+                                cacheTimeout = 60000;
+                            if (typeof cache[key] == 'undefined' || (cacheTimeout && Date.now() >= cache[key].time)) {
+                                $.ajax(params).fail(failure).done(function (data) {
+                                    cache[key] = {
+                                        data: data,
+                                        time: cacheTimeout ? Date.now() + cacheTimeout : null
+                                    };
+                                    success(data);
+                                });
+                            } else {
+                                success(cache[key].data);
+                            }
+                        } else {
+                            $.ajax(params).fail(failure).done(success);
+                        }
+                    },
+                    data: function(params) {
+                        return {
+                            q: params
+                        };
+                    },
+                    results: function(data) {
+                        var results, more = false, response = {};
+
+                        if ($.isArray(data)) {
+                            results = data;
+                        } else if (typeof data == 'object') {
+                            // assume remote result was proper object
+                            results = data.results;
+                            more = data.more;
+                        } else {
+                            // failsafe
+                            results = [];
+                        }
+                        response.results = results;
+
+                        return response;
                     }
-                    var text = state.text;
-
-                    text = text.replace('|_', '&nbsp;');
-                    text = text.replace(/^[\_]+/, function(found) {
-                        return new Array(found.length * 4).join('&nbsp;');
-                    });
-
-                    return text;
                 };
-            } else if (!('formatResult' in options)) {
-                options.formatResult = function(state) {
-                    return '<span btn-select2-value="'+state.id+'">'+state.text+'</span>';
-                };
-            }
-
-            if (!('formatSelection' in options)) {
-                options.formatSelection = function(state) {
-                    var parent = $(state.element).parent();
-                    var text = state.text.replace(/^[\_|]+/, '');
-                    if ('OPTGROUP' === parent.prop('tagName') && parent.attr('label')) {
-                        return  text + ' <small class="optgroup">('+parent.attr('label')+')</small>';
+                options.initSelection = function(element, callback) {
+                    var id = element.attr('btn-select2-ajax-id'),
+                        text = element.attr('btn-select2-ajax-text');
+                    if(typeof id != 'undefined'){
+                        element.val(id);
+                        callback({ id: id, text: text });
+                    }else{
+                        callback();
                     }
-
-                    return text;
-                };
+                }
             }
 
             element.select2(options);
